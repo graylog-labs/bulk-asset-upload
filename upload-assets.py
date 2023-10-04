03206ce2c2a2c3003e3f03b89c262d9c33661ff9
Rob Curtis <rob.curtis@graylog.com>
2023-10-04 15:15:43 -0400

    Now it's working

--- upload-assets.py
+++ upload-assets.py
@@ -8,7 +8,7 @@ from logging.handlers import TimedRotatingFileHandler
 
 handlers = set()
 handlers.add(TimedRotatingFileHandler('asset-upload.log', when='W0', backupCount=4))
-logging.basicConfig(level=logging.DEBUG, handlers=handlers, 
+logging.basicConfig(level=logging.INFO, handlers=handlers, 
                     format='%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s')
 logging.Formatter.formatTime = (lambda self, record, datefmt=None: 
     datetime.fromtimestamp(record.created, timezone.utc).astimezone().isoformat(sep='T',timespec='milliseconds'))
@@ -16,10 +16,10 @@ logging.Formatter.formatTime = (lambda self, record, datefmt=None:
 def split_and_strip(input: str) -> list:
     return [x.strip() for x in input.split(',')]
 
-def do_post(request_url: str, api_key: str, record: dict, request_headers: dict={}) -> dict:
-    logging.debug(f'Invoking the API with API key {api_key}')
+def do_post(request_url: str, api_key: str, record: dict, request_headers: dict={'X-Requested-By': 'bulk upload script'}) -> dict:
     endpoint = '/api/plugins/org.graylog.plugins.securityapp.asset/assets'
     try:
+        logging.debug(f'Invoking the asset creation API with URL {request_url+endpoint} and API key {api_key}')
         result = requests.post(
             request_url + endpoint,
             headers = request_headers,
@@ -27,13 +27,14 @@ def do_post(request_url: str, api_key: str, record: dict, request_headers: dict=
             json = record
             )
     except requests.exceptions.RequestException as e:
-        logging.critical('Error %s submitting API call', e)
+        logging.exception('Error submitting API call')
         return
 
     if result.status_code != 200:
         logging.critical(
-            'Unable to perform API call - error code %s',
-            result.status_code
+            'Unable to perform API call - HTTP code %s: %s',
+            result.status_code,
+            result.text
             )
         return
 
@@ -44,23 +45,23 @@ def build_record(csv_row:dict) -> dict:
     logging.debug(f'Building record for {csv_row[0]} with IPs [{csv_row[6]}] owned by {csv_row[2]}')
     record = {}
     record['name'] = csv_row[0]
-    record['description'] = csv_row[1]
     record['category'] = split_and_strip(csv_row[7])
     record['priority'] = csv_row[3]
     record['details'] = {}
+    record['details']['description'] = csv_row[1]
     record['details']['type'] = 'machine'
     record['details']['owner'] = csv_row[2]
     record['details']['hostnames'] = split_and_strip(csv_row[4])
     record['details']['mac_addresses'] = split_and_strip(csv_row[5])
     record['details']['ip_addresses'] = split_and_strip(csv_row[6])
-    record['geo_info'] = {}
-    record['geo_info']['city_name'] = csv_row[8]
-    record['geo_info']['region'] = csv_row[9]
-    record['geo_info']['country_name'] = csv_row[10]
-    record['geo_info']['latitude'] = csv_row[11]
-    record['geo_info']['longitude'] = csv_row[12]
-    record['geo_info']['country_iso_code'] = csv_row[13]
-    record['geo_info']['time_zone'] = csv_row[14]
+    record['details']['geo_info'] = {}
+    record['details']['geo_info']['city_name'] = csv_row[8]
+    record['details']['geo_info']['region'] = csv_row[9]
+    record['details']['geo_info']['country_name'] = csv_row[10]
+    record['details']['geo_info']['latitude'] = csv_row[11]
+    record['details']['geo_info']['longitude'] = csv_row[12]
+    record['details']['geo_info']['country_iso_code'] = csv_row[13]
+    record['details']['geo_info']['time_zone'] = csv_row[14]
 
     logging.debug(f'Generated record:\n {json.dumps(record, indent=2)}')
 
@@ -92,6 +93,7 @@ def main():
         csv_reader = csv.reader(csv_file, delimiter=',')
         header_skipped = False
         records_read = 0
+        records_created = 0
 
         for record in csv_reader:
             if not header_skipped and parsed_args.noheader:
@@ -103,11 +105,15 @@ def main():
             asset_record = build_record(record)
             
             # PUT the record to the API
-            do_post(request_url=request_url, api_key=parsed_args.apikey, record=asset_record, request_headers={})
+            result = do_post(request_url=request_url, api_key=parsed_args.apikey, record=asset_record)
+
+            if 'id' in result:
+                logging.info(f'New record created with id {result["id"]}')
+                records_created += 1
 
             records_read += 1
 
-        logging.info(f'Handled {records_read} records -- DONE')
+        logging.info(f'Created {records_created} new assets from {records_read} records -- DONE')
 
 if __name__ == '__main__':
     main()
