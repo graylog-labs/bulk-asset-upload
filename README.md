e92465f9a9b4bc0ae5bd272f67a40fec8f4078ed
Rob Curtis <rob.curtis@graylog.com>
2023-10-05 11:24:33 -0400

    Add support for user asset upload

--- README.md
+++ README.md
@@ -1,32 +1,30 @@
 # Disclaimer
 This script is provided with no guarantees, warranties, or herbal teas. It was written in a hurry by someone with no professional Python coding experience. It worked in my very limited testing, but it might require tweaks or updates to work for you.
 
-**Note**: The script currently only supports uploading machine assets. User asset upload may come in the future.
-
 # Overview
 Graylog 5.2 includes a new Asset Database plugin which allows you to define machine and user assets and correlate those assets with your incoming log data. At release, it supports entering assets manually or importing from either LDAP or Active Directory. However, many people who don't have an LDAP or AD server do have way more machines than they care to enter manually. So I built this script to import machine assets in bulk from a .csv file.
 
 # Instructions
-The most important thing to mention is that the script expects your .csv file to be organized exactly like the provided `sample_machine_upload.csv` file. The first column must be the machine name. The second column must be the description. And so on out to the 15th column for time zone.
+The most important thing to mention is that the script expects your .csv file to be organized exactly like the provided `sample_machine_upload.csv` and `sample_user_upload.csv` files. If the data is not in the expected columns, the script will not work.
 
-I highly recommend you use `sample_machine_upload.csv` as a starting point. Load it up in your favorite spreadsheet program, delete rows 2-6, and fill in with your own data.
+I highly recommend you use `sample_machine_upload.csv` or `sample_user_upload.csv` as a starting point. Load it up in your favorite spreadsheet program, delete the non-header rows, and fill in with your own data.
 
 ## python3 upload-assets.py -h
 ```
-usage: upload-assets.py [-h] [--https] [-s SERVER] [-p PORT] -k APIKEY -f FILE [--noheader]
+usage: upload-assets.py [-h] (--user | --machine) [--https] [-s SERVER] [-p PORT] -k APIKEY -f FILE [--noheader]
 
 Bulk import assets from a .csv file to Graylog Security
 
 optional arguments:
   -h, --help            show this help message and exit
+  --user                Upload user assets
+  --machine             Upload machine assets
   --https               Use https (as opposed to http)
   -s SERVER, --server SERVER
                         The host where Graylog is running (default to localhost)
-  -p PORT, --port PORT  The port Graylog is listening on (default to 9000)
+  -p PORT, --port PORT  The port Graylog is listening on (default to 9000 for HTTP, not used for HTTPS)
   -k APIKEY, --apikey APIKEY
                         The API key for accessing the Graylog API
   -f FILE, --file FILE  The .csv file to read data from
-  --noheader            The .csv file has column headers per the documentation
+  --noheader            The .csv file has no column headers, there is data in the first row
 ```
-
-
--- upload-assets.py
+++ upload-assets.py
@@ -41,8 +41,8 @@ def do_post(request_url: str, api_key: str, record: dict, request_headers: dict=
     logging.debug('API call successful')
     return result.json()
 
-def build_record(csv_row:dict) -> dict:
-    logging.debug(f'Building record for {csv_row[0]} with IPs [{csv_row[6]}] owned by {csv_row[2]}')
+def build_machine_record(csv_row:dict) -> dict:
+    logging.debug(f'Building machine record for {csv_row[0]} with IPs [{csv_row[6]}] owned by {csv_row[2]}')
     record = {}
     record['name'] = csv_row[0]
     record['category'] = split_and_strip(csv_row[7])
@@ -67,25 +67,45 @@ def build_record(csv_row:dict) -> dict:
 
     return record
 
+def build_user_record(csv_row:dict) -> dict:
+    logging.debug(f'Building user record for {csv_row[1]} {csv_row[2]} with email addresses {csv_row[3]}')
+    record = {}
+    record['name'] = csv_row[0]
+    record['category'] = split_and_strip(csv_row[7])
+    record['priority'] = csv_row[6]
+    record['details'] = {}
+    record['details']['type'] = 'user'
+    record['details']['usernames'] = split_and_strip(csv_row[4])
+    record['details']['user_ids'] = split_and_strip(csv_row[5])
+    record['details']['email_addresses'] = split_and_strip(csv_row[3])
+    record['details']['first_name'] = csv_row[1]
+    record['details']['last_name'] = csv_row[2]
+
+    logging.debug(f'Generated record:\n {json.dumps(record, indent=2)}')
+
+    return record
+
 def main():
-    logging.info('Starting machine asset upload')
+    logging.info('Starting asset upload')
     parser = argparse.ArgumentParser(description='Bulk import assets from a .csv file to Graylog Security')
+    arg_group = parser.add_mutually_exclusive_group(required=True)
+    arg_group.add_argument('--user', action='store_true', help='Upload user assets')
+    arg_group.add_argument('--machine', action='store_true', help='Upload machine assets')
     parser.add_argument('--https', action='store_true', help='Use https (as opposed to http)')
     parser.add_argument('-s', '--server', default='localhost', help='The host where Graylog is running (default to localhost)')
-    parser.add_argument('-p', '--port', default='9000', help='The port Graylog is listening on (default to 9000)')
+    parser.add_argument('-p', '--port', default='9000', help='The port Graylog is listening on (default to 9000 for HTTP, not used for HTTPS)')
     parser.add_argument('-k', '--apikey', required=True, help='The API key for accessing the Graylog API')
     parser.add_argument('-f', '--file', required=True, help='The .csv file to read data from')
-    parser.add_argument('--noheader', action='store_false', help='The .csv file has column headers per the documentation')
+    parser.add_argument('--noheader', action='store_false', help='The .csv file has no column headers, there is data in the first row')
 
     logging.debug('Parsing command line args')
     parsed_args = parser.parse_args()
 
     request_url = ''
     if parsed_args.https:
-        request_url = 'https://'
+        request_url = f'https://{parsed_args.server}'
     else:
-        request_url = 'http://'
-    request_url += f'{parsed_args.server}:{parsed_args.port}'
+        request_url = f'http://{parsed_args.server}:{parsed_args.port}'
     logging.debug(f'Request URL: {request_url}')
 
     logging.debug(f'Opening input file: {parsed_args.file}')
@@ -102,9 +122,13 @@ def main():
                 continue
             
             # Parse a row of data
-            asset_record = build_record(record)
+            asset_record = {}
+            if parsed_args.user:
+                asset_record = build_user_record(record)
+            else:
+                asset_record = build_machine_record(record)
             
-            # PUT the record to the API
+            # POSTT the record to the API
             result = do_post(request_url=request_url, api_key=parsed_args.apikey, record=asset_record)
 
             if 'id' in result:
